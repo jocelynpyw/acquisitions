443d3fbcbb902aecd186005ff2f687122c085c74
Jocelyn Yemaleu <jocelynwotcheu2@gmail.com>
2025-12-08 22:46:58 +0100

    user tables creates

--- drizzle/0000_military_leader.sql
+++ drizzle/0000_military_leader.sql
@@ -8,3 +8,13 @@ CREATE TABLE "users" (
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" varchar(50) DEFAULT 'user' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
