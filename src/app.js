0f99c1a60f8a78a69977f61b8a29ba8722752698
Jocelyn Yemaleu <jocelynwotcheu2@gmail.com>
2026-01-26 00:33:21 +0100

    middleware security setup

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 app.use(helmet());
@@ -17,6 +18,8 @@ app.use(
     stream: { write: message => logger.info(message.trim()) },
   })
 );
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from acquisition !');
--- src/middleware/security.middleware.js
+++ src/middleware/security.middleware.js
@@ -1,6 +1,8 @@
 import aj from '#config/arcjet.js';
+import logger from '#config/logger.js';
+import { slidingWindow } from '@arcjet/node';
 
-const arcjetMiddleware = async (req, res, next) => {
+const securityMiddleware = async (req, res, next) => {
   try {
     const role = req.user?.role || 'guest';
 
@@ -10,20 +12,64 @@ const arcjetMiddleware = async (req, res, next) => {
     switch (role) {
       case 'admin':
         limit = 20;
-        message =
-          'Admin request limit exceeded (20 requests per minute). Slow down.';
+        message = 'Admin request limit exceeded (20 per minute). Slow down.';
         break;
       case 'user':
         limit = 10;
-        message =
-          'User request limit exceeded (10 requests per minute). Slow down.';
+        message = 'User request limit exceeded (10 per minute). Slow down.';
         break;
       case 'guest':
         limit = 5;
-        message =
-          'Guest request limit exceeded (5 requests per minute). Slow down.';
+        message = 'Guest request limit exceeded (5 per minute). Slow down.';
         break;
     }
+    const client = aj.withRule(
+      slidingWindow({
+        mode: 'LIVE',
+        interval: '1m',
+        max: limit,
+        name: `${role}-rate-limit`,
+      })
+    );
+    const decision = await client.protect(req);
+
+    // if (decision.isDenied() && decision.reason.isBot()) {
+    //   logger.warn('Bot request blocked', {
+    //     ip: req.ip,
+    //     userAgent: req.get('User-Agent'),
+    //     path: req.path,
+    //   });
+    //   return res.status(403).json({
+    //     error: 'Forbidden',
+    //     message: 'Automatic requests are not allowed',
+    //   });
+    // }
+
+    if (decision.isDenied() && decision.reason.isShield()) {
+      logger.warn('Shield request blocked', {
+        ip: req.ip,
+        userAgent: req.get('User-Agent'),
+        path: req.path,
+        method: req.method,
+      });
+      return res.status(403).json({
+        error: 'Forbidden',
+        message: 'Request blocked by security policy',
+      });
+    }
+
+    if (decision.isDenied() && decision.reason.isRateLimit()) {
+      logger.warn('Rate limit exceded', {
+        ip: req.ip,
+        userAgent: req.get('User-Agent'),
+        path: req.path,
+      });
+      return res.status(403).json({
+        error: 'Forbidden',
+        message: 'Too many requests',
+      });
+    }
+    next();
   } catch (e) {
     console.error('Arcjet middleware error:', e);
     res.status(500).json({
@@ -32,3 +78,5 @@ const arcjetMiddleware = async (req, res, next) => {
     });
   }
 };
+
+export default securityMiddleware;
